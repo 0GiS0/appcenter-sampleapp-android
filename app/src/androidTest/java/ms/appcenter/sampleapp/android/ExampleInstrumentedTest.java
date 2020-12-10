05e409fcce017950fac3f6ea2ee98da00a0309c4
0GiS0 <gisela.torres@returngis.net>
2020-12-10 14:47:27 +0100

    added new test

--- app/src/androidTest/java/ms/appcenter/sampleapp/android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ms/appcenter/sampleapp/android/ExampleInstrumentedTest.java
@@ -2,18 +2,28 @@ package ms.appcenter.sampleapp.android;
 
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Rule;
 import org.junit.After;
-import com.microsoft.appcenter.espresso.Factory;
-import com.microsoft.appcenter.espresso.ReportHelper;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
+
+/* Espresso */
+
+import com.microsoft.appcenter.espresso.Factory;
+import com.microsoft.appcenter.espresso.ReportHelper;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import androidx.test.espresso.assertion.ViewAssertions.*;
+
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -22,6 +32,10 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityActivityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
     @Rule
     public ReportHelper reportHelper = Factory.getReportHelper();
 
@@ -37,4 +51,14 @@ public class ExampleInstrumentedTest {
 
         assertEquals("ms.appcenter.sampleapp.android", appContext.getPackageName());
     }
+
+    @Test
+    public void welcomeText_newActivity(){
+
+        //Check that the text is "Visual Studio App Center is mission control for apps. It brings together multiple services, commonly used for mobile developers, into a single, integrated product.\n\n\nSwipe right to learn about our services"
+        //onView(withId(R.id.welcomeTextView)).check(matches(withText("Visual Studio App Center is mission control for apps.")));
+        onView(withId(R.id.welcomeTextView)).check(matches(withText("Visual Studio App Center is mission control for apps. It brings together multiple services, commonly used for mobile developers, into a single, integrated product.\n\n\nSwipe right to learn about our services")));
+
+    }
 }
+
